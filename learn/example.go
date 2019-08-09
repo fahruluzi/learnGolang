c0c8947f050bc17d4ce508f0c6b921fc07e5be30
Fahrul Fauzi <fahrulf594@gmail.com>
2019-08-09 10:19:03 +0700

    Variabel + tipe data string

--- learn/example.go
+++ learn/example.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 )
 
+// Variabel - Tipe Data : String
+var pelajaran1 = "Go"
+var pelajaran2 = "Dart"
+
+var namaDepan = "Fahrul"
+var namaBelakang = "Fauzi"
+var namaLengkap = namaDepan + " " + namaBelakang
+
 func main() {
-	fmt.Println("Tayooo")
+	fmt.Println("Saya belajar bahasa " + pelajaran1 + " Dan " + pelajaran2)
+	fmt.Println("Nama Saya " + namaLengkap)
 }
